c4a0db6452a78be4b9eb4537dc372e281c237e6f
Fabian Ferno <fabianferno@gmail.com>
2021-04-22 19:05:21 +0530

    Init firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/analytics";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiByTao77dbcDXhyun89Zz7C9d8sp-G_A",
+  authDomain: "reactor-pad.firebaseapp.com",
+  projectId: "reactor-pad",
+  storageBucket: "reactor-pad.appspot.com",
+  messagingSenderId: "72088684880",
+  appId: "1:72088684880:web:db60950b1f5a2f6e1d8455",
+  measurementId: "G-675LX7WGY6",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,24 @@
+import Button from "@material-ui/core/Button";
+
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <section>
+      <div
+        className="container d-flex align-items-center justify-content-center flex-column"
+        style={{ minHeight: "100vh" }}
+      >
+        <h1 className="text-primary">
+          <strong>reactor</strong>-pad-app
+        </h1>
+        <Button
+          variant="contained"
+          color="primary"
+          className="mt-3"
+          style={{ textTransform: "capitalize" }}
+        >
+          Sign in with Google
+        </Button>
+      </div>
+    </section>
+  );
 }
